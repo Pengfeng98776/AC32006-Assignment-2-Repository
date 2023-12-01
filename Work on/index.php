ddf136ddf3c3b991079e2ecfe091494e2a776d23
Naomi Silver <2425281@dundee.ac.uk>
2023-12-01 13:19:57 +0000

    Added random fetch on index.php
    added random cards for featured products <3

--- Work on/index.php
+++ Work on/index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <!-- include head for page -->
-  <?php include 'includes/head.php';?>
+  <?php include 'includes/head.php'; ?>
 <body>
   <?php
   include 'includes/db.php';
   include 'includes/navbar.php';
+  $stmt = $mysql->query("SELECT * FROM Product ORDER BY RAND() LIMIT 3");
+  $products = $stmt->fetchAll(PDO::FETCH_ASSOC); // an attempt at stopping duplicate randoms lmao
   ?>
 
   <section class="cover">
@@ -17,31 +19,30 @@
   </section>
 
   <section class="featured-products container mt-5">
-    <h2 class="text-center mb-4">Featured Bicycles</h2>
+    <h2 class="text-center mb-4">Featured Products</h2>
     <div class="row row-cols-1 row-cols-md-3 g-4">
-
-      <div class="col mt-2">
-        <div class="card h-100">
-          <img class="card-img-top" src="images/product placeholders/bike-image-1.jpg" alt="Mountain Bike">
-          <div class="card-body">
-            <div class="row">
-              <div class="col-8">
-                <h5 class="card-title">Santa Cruz Tallboy</h5>
+      <?php foreach ($products as $result): ?>
+        <div class="col mt-2">
+          <div class="card h-100">
+            <img class="card-img-top" src="images/product placeholders/bike-image-1.jpg" alt="A placeholder image">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-8">
+                  <h5 class="card-title"><?php echo $result['Name']; ?></h5>
+                </div>
+                <div class="col-4">
+                  <h5 class="card-title">£<?php echo $result['PurchaseCost']; ?></h5>
+                </div>
               </div>
-              <div class="col-4">
-                <h5 class="card-title">£6,599</h5>
-              </div>
+              <p class="card-text crop-text-2"><?php echo $result['Description']; ?></p>
+              <a href="#" class="btn btn-outline-success col-12">Add to cart</a>
             </div>
-            <p class="card-text crop-text-2">One of the first 29er full sussers that worked a trail bike, the every man's bike.</p>
-            <a href="#" class="btn btn-outline-success col-12">Add to cart</a>
           </div>
         </div>
-      </div>
-      <!-- above is just a placeholder, could use sales to figure out what to feature?? -->
-      <!-- also use the old card, we need to adapt them to include price and stuff :)) -->
+      <?php endforeach; ?>
     </div>
   </section>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMneT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
--- Work on/manager.php
+++ Work on/manager.php
@@ -10,7 +10,7 @@
   // SELECTS ALL PRODUCTS
   $query = "SELECT * FROM Product";
   $stmt = $mysql->prepare($query);
-  $stmt->execute(); 
+  $stmt->execute();
   ?>
     <div class="container-fluid">
       <div class="row">
@@ -19,14 +19,13 @@
           <div id="products" class="row mt-4 row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4"> <!-- cards generated from query & js magic goes here, currently hardcoded examples -->
 
           <?php $result = $stmt->fetch(); ?>
-          <?php if($result != NULL): ?>
             <div class="col mt-2">
               <div class="card">
                 <img class="card-img-top" src="images/product placeholders/product placeholder.jpg" alt="A placeholder image">
                 <div class="card-body">
                   <div class="row">
                     <div class="col-8">
-                      <h5 class="card-title"><?php echo $result['ProductID'], ': ', $result['Name'] ?></h5>
+                      <h5 class="card-title"><?php echo $result['Name'] ?></h5>
                     </div>
                     <div class="col-4">
                       <h5 class="card-title">£<?php echo $result['PurchaseCost'] ?></h5>
@@ -40,17 +39,15 @@
                 </div>
               </div>
             </div>
-            <?php endif; ?>
 
             <?php $result = $stmt->fetch(); ?>
-            <?php if($result != NULL): ?>
             <div class="col mt-2">
               <div class="card">
                 <img class="card-img-top" src="images/product placeholders/product placeholder sold out.jpg" alt="A placeholder image" >
                 <div class="card-body">
                   <div class="row">
                     <div class="col-8">
-                    <h5 class="card-title"><?php echo $result['ProductID'], ': ', $result['Name'] ?></h5>
+                      <h5 class="card-title"><?php echo $result['Name'] ?></h5>
                     </div>
                     <div class="col-4">
                       <h5 class="card-title">£<?php echo $result['PurchaseCost'] ?></h5>
@@ -64,17 +61,15 @@
                 </div>
               </div>
             </div>
-            <?php endif; ?>
 
             <?php $result = $stmt->fetch(); ?>
-            <?php if($result != NULL): ?>
             <div class="col mt-2">
               <div class="card">
                 <img class="card-img-top" src="images/product placeholders/product placeholder.jpg" alt="A placeholder image" >
                 <div class="card-body">
                   <div class="row">
                     <div class="col-8">
-                    <h5 class="card-title"><?php echo $result['ProductID'], ': ', $result['Name'] ?></h5>
+                      <h5 class="card-title"><?php echo $result['Name'] ?></h5>
                     </div>
                     <div class="col-4">
                       <h5 class="card-title">£<?php echo $result['PurchaseCost'] ?></h5>
@@ -88,17 +83,15 @@
                 </div>
               </div>
             </div>
-            <?php endif; ?>
 
             <?php $result = $stmt->fetch(); ?>
-            <?php if($result != NULL): ?>
             <div class="col mt-2">
               <div class="card">
                 <img class="card-img-top" src="images/product placeholders/product placeholder.jpg" alt="A placeholder image" >
                 <div class="card-body">
                   <div class="row">
                     <div class="col-8">
-                    <h5 class="card-title"><?php echo $result['ProductID'], ': ', $result['Name'] ?></h5>
+                      <h5 class="card-title"><?php echo $result['Name'] ?></h5>
                     </div>
                     <div class="col-4">
                       <h5 class="card-title">£<?php echo $result['PurchaseCost'] ?></h5>
@@ -112,17 +105,15 @@
                 </div>
               </div>
             </div>
-            <?php endif; ?>
 
             <?php $result = $stmt->fetch(); ?>
-            <?php if($result != NULL): ?>
             <div class="col mt-2">
               <div class="card">
                 <img class="card-img-top" src="images/product placeholders/product placeholder sold out.jpg" alt="A placeholder image" >
                 <div class="card-body">
                   <div class="row">
                     <div class="col-8">
-                    <h5 class="card-title"><?php echo $result['ProductID'], ': ', $result['Name'] ?></h5>
+                      <h5 class="card-title"><?php echo $result['Name'] ?></h5>
                     </div>
                     <div class="col-4">
                       <h5 class="card-title">£<?php echo $result['PurchaseCost'] ?></h5>
@@ -136,17 +127,15 @@
                 </div>
               </div>
             </div>
-            <?php endif; ?>
 
             <?php $result = $stmt->fetch(); ?>
-            <?php if($result != NULL): ?>
             <div class="col mt-2">
               <div class="card">
                 <img class="card-img-top" src="images/product placeholders/product placeholder.jpg" alt="A placeholder image" >
                 <div class="card-body">
                   <div class="row">
                     <div class="col-8">
-                    <h5 class="card-title"><?php echo $result['ProductID'], ': ', $result['Name'] ?></h5>
+                      <h5 class="card-title"><?php echo $result['Name'] ?></h5>
                     </div>
                     <div class="col-4">
                       <h5 class="card-title">£<?php echo $result['PurchaseCost'] ?></h5>
@@ -160,17 +149,15 @@
                 </div>
               </div>
             </div>
-            <?php endif; ?>
 
             <?php $result = $stmt->fetch(); ?>
-            <?php if($result != NULL): ?>
             <div class="col mt-2">
               <div class="card">
                 <img class="card-img-top" src="images/product placeholders/product placeholder.jpg" alt="A placeholder image" >
                 <div class="card-body">
                   <div class="row">
                     <div class="col-8">
-                    <h5 class="card-title"><?php echo $result['ProductID'], ': ', $result['Name'] ?></h5>
+                      <h5 class="card-title"><?php echo $result['Name'] ?></h5>
                     </div>
                     <div class="col-4">
                       <h5 class="card-title">£<?php echo $result['PurchaseCost'] ?></h5>
@@ -184,17 +171,15 @@
                 </div>
               </div>
             </div>
-            <?php endif; ?>
 
             <?php $result = $stmt->fetch(); ?>
-            <?php if($result != NULL): ?>
             <div class="col mt-2">
               <div class="card">
                 <img class="card-img-top" src="images/product placeholders/product placeholder sold out.jpg" alt="A placeholder image" >
                 <div class="card-body">
                   <div class="row">
                     <div class="col-8">
-                    <h5 class="card-title"><?php echo $result['ProductID'], ': ', $result['Name'] ?></h5>
+                      <h5 class="card-title"><?php echo $result['Name'] ?></h5>
                     </div>
                     <div class="col-4">
                       <h5 class="card-title">£<?php echo $result['PurchaseCost'] ?></h5>
@@ -208,7 +193,6 @@
                 </div>
               </div>
             </div>
-            <?php endif; ?>
           </div>
         </div>
 
@@ -219,7 +203,7 @@
               <h3><i class="fa-solid fa-cart-shopping"></i>Add new product:</h3>
               <div class="row">
                 <div class="col">
-                  
+
                 </div>
               </div>
               <hr class="hr hr-blurry mt-2"/>
@@ -234,7 +218,7 @@
                   <input type="text" class="form-control" name="name" placeholder="Name">
                 </div>
                 <p></p> <!-- spacing smile -->
-                
+                </div>
                 <div class="form-group">
                   <label for="exampleFormControlTextarea1">Product Description:</label>
                   <textarea class="form-control" name="description" rows="3"></textarea>
@@ -259,23 +243,9 @@
                 </div>
     </form>
             </div>
-</div>
-            <div class="row mt-5 ms-3 me-3">
-              <form name="Remove New Product" action="removeProduct.php" method="post">
-                <h4>Delete Product:</h4>
-                <input type="text" class="form-control" name="delID" placeholder="ID to delete">
-                <input type="submit" name="submitDel" value="Confirm" class="btn btn-outline-success col-12 mt-2"></input>
-              </form>
-            </div>
           </div>
       </div>
     </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
-
-
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
