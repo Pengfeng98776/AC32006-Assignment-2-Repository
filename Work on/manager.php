7f35b92bfde9ed41856bd407f1a17d4a1e931a0e
Soproxi <zoey.mccall25@gmail.com>
2023-12-01 11:14:40 +0000

    added error handling to manager page

--- Work on/manager.php
+++ Work on/manager.php
@@ -10,7 +10,7 @@
   // SELECTS ALL PRODUCTS
   $query = "SELECT * FROM Product";
   $stmt = $mysql->prepare($query);
-  $stmt->execute();
+  $stmt->execute(); 
   ?>
     <div class="container-fluid">
       <div class="row">
@@ -19,6 +19,7 @@
           <div id="products" class="row mt-4 row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4"> <!-- cards generated from query & js magic goes here, currently hardcoded examples -->
 
           <?php $result = $stmt->fetch(); ?>
+          <?php if($result != NULL): ?>
             <div class="col mt-2">
               <div class="card">
                 <img class="card-img-top" src="images/product placeholders/product placeholder.jpg" alt="A placeholder image">
@@ -39,8 +40,10 @@
                 </div>
               </div>
             </div>
+            <?php endif; ?>
 
             <?php $result = $stmt->fetch(); ?>
+            <?php if($result != NULL): ?>
             <div class="col mt-2">
               <div class="card">
                 <img class="card-img-top" src="images/product placeholders/product placeholder sold out.jpg" alt="A placeholder image" >
@@ -61,8 +64,10 @@
                 </div>
               </div>
             </div>
+            <?php endif; ?>
 
             <?php $result = $stmt->fetch(); ?>
+            <?php if($result != NULL): ?>
             <div class="col mt-2">
               <div class="card">
                 <img class="card-img-top" src="images/product placeholders/product placeholder.jpg" alt="A placeholder image" >
@@ -83,8 +88,10 @@
                 </div>
               </div>
             </div>
+            <?php endif; ?>
 
             <?php $result = $stmt->fetch(); ?>
+            <?php if($result != NULL): ?>
             <div class="col mt-2">
               <div class="card">
                 <img class="card-img-top" src="images/product placeholders/product placeholder.jpg" alt="A placeholder image" >
@@ -105,8 +112,10 @@
                 </div>
               </div>
             </div>
+            <?php endif; ?>
 
             <?php $result = $stmt->fetch(); ?>
+            <?php if($result != NULL): ?>
             <div class="col mt-2">
               <div class="card">
                 <img class="card-img-top" src="images/product placeholders/product placeholder sold out.jpg" alt="A placeholder image" >
@@ -127,8 +136,10 @@
                 </div>
               </div>
             </div>
+            <?php endif; ?>
 
             <?php $result = $stmt->fetch(); ?>
+            <?php if($result != NULL): ?>
             <div class="col mt-2">
               <div class="card">
                 <img class="card-img-top" src="images/product placeholders/product placeholder.jpg" alt="A placeholder image" >
@@ -149,8 +160,10 @@
                 </div>
               </div>
             </div>
+            <?php endif; ?>
 
             <?php $result = $stmt->fetch(); ?>
+            <?php if($result != NULL): ?>
             <div class="col mt-2">
               <div class="card">
                 <img class="card-img-top" src="images/product placeholders/product placeholder.jpg" alt="A placeholder image" >
@@ -171,8 +184,10 @@
                 </div>
               </div>
             </div>
+            <?php endif; ?>
 
             <?php $result = $stmt->fetch(); ?>
+            <?php if($result != NULL): ?>
             <div class="col mt-2">
               <div class="card">
                 <img class="card-img-top" src="images/product placeholders/product placeholder sold out.jpg" alt="A placeholder image" >
@@ -193,6 +208,7 @@
                 </div>
               </div>
             </div>
+            <?php endif; ?>
           </div>
         </div>
 
@@ -203,7 +219,7 @@
               <h3><i class="fa-solid fa-cart-shopping"></i>Add new product:</h3>
               <div class="row">
                 <div class="col">
-
+                  
                 </div>
               </div>
               <hr class="hr hr-blurry mt-2"/>
@@ -246,6 +262,12 @@
           </div>
       </div>
     </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+
+
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
