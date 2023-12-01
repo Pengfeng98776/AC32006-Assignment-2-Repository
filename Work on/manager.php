cb12498a599bf2c4033440c38b3d9329d6142331
Matthew Gibson <2425530@dundee.ac.uk>
2023-12-01 13:45:13 +0000

    re-added for statement cause it got nuked by merge

--- Work on/manager.php
+++ Work on/manager.php
@@ -6,11 +6,10 @@
   <?php
   include 'includes/db.php';
   include 'includes/navbar.php';
-
   // SELECTS ALL PRODUCTS
   $query = "SELECT * FROM Product";
   $stmt = $mysql->prepare($query);
-  $stmt->execute();
+  $stmt->execute(); 
   ?>
     <div class="container-fluid">
       <div class="row">
@@ -27,7 +26,7 @@
                 <div class="card-body">
                   <div class="row">
                     <div class="col-8">
-                      <h5 class="card-title"><?php echo $result['Name'] ?></h5>
+                      <h5 class="card-title"><?php echo $result['ProductID'], ': ', $result['Name'] ?></h5>
                     </div>
                     <div class="col-4">
                       <h5 class="card-title">£<?php echo $result['PurchaseCost'] ?></h5>
@@ -41,171 +40,19 @@
                 </div>
               </div>
             </div>
-
-            <?php $result = $stmt->fetch(); ?>
-            <div class="col mt-2">
-              <div class="card">
-                <img class="card-img-top" src="images/product placeholders/product placeholder sold out.jpg" alt="A placeholder image" >
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-8">
-                      <h5 class="card-title"><?php echo $result['Name'] ?></h5>
-                    </div>
-                    <div class="col-4">
-                      <h5 class="card-title">£<?php echo $result['PurchaseCost'] ?></h5>
-                    </div>
-                  </div>
-                  <p class="card-text crop-text-2"><?php echo $result['Description'] ?></p>
-                  <div class="row">
-                    <a href="#" class="btn btn-outline-warning col-6" >Mark as in-stock</a>
-                    <a href="#" class="btn btn-outline-primary col-6" >Generate report</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <?php $result = $stmt->fetch(); ?>
-            <div class="col mt-2">
-              <div class="card">
-                <img class="card-img-top" src="images/product placeholders/product placeholder.jpg" alt="A placeholder image" >
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-8">
-                      <h5 class="card-title"><?php echo $result['Name'] ?></h5>
-                    </div>
-                    <div class="col-4">
-                      <h5 class="card-title">£<?php echo $result['PurchaseCost'] ?></h5>
-                    </div>
-                  </div>
-                  <p class="card-text crop-text-2"><?php echo $result['Description'] ?></p>
-                  <div class="row">
-                    <a href="#" class="btn btn-outline-warning col-6" >Mark as in-stock</a>
-                    <a href="#" class="btn btn-outline-primary col-6" >Generate report</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <?php $result = $stmt->fetch(); ?>
-            <div class="col mt-2">
-              <div class="card">
-                <img class="card-img-top" src="images/product placeholders/product placeholder.jpg" alt="A placeholder image" >
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-8">
-                      <h5 class="card-title"><?php echo $result['Name'] ?></h5>
-                    </div>
-                    <div class="col-4">
-                      <h5 class="card-title">£<?php echo $result['PurchaseCost'] ?></h5>
-                    </div>
-                  </div>
-                  <p class="card-text crop-text-2"><?php echo $result['Description'] ?></p>
-                  <div class="row">
-                    <a href="#" class="btn btn-outline-warning col-6" >Mark as in-stock</a>
-                    <a href="#" class="btn btn-outline-primary col-6" >Generate report</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <?php $result = $stmt->fetch(); ?>
-            <div class="col mt-2">
-              <div class="card">
-                <img class="card-img-top" src="images/product placeholders/product placeholder sold out.jpg" alt="A placeholder image" >
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-8">
-                      <h5 class="card-title"><?php echo $result['Name'] ?></h5>
-                    </div>
-                    <div class="col-4">
-                      <h5 class="card-title">£<?php echo $result['PurchaseCost'] ?></h5>
-                    </div>
-                  </div>
-                  <p class="card-text crop-text-2"><?php echo $result['Description'] ?></p>
-                  <div class="row">
-                    <a href="#" class="btn btn-outline-warning col-6" >Mark as in-stock</a>
-                    <a href="#" class="btn btn-outline-primary col-6" >Generate report</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <?php $result = $stmt->fetch(); ?>
-            <div class="col mt-2">
-              <div class="card">
-                <img class="card-img-top" src="images/product placeholders/product placeholder.jpg" alt="A placeholder image" >
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-8">
-                      <h5 class="card-title"><?php echo $result['Name'] ?></h5>
-                    </div>
-                    <div class="col-4">
-                      <h5 class="card-title">£<?php echo $result['PurchaseCost'] ?></h5>
-                    </div>
-                  </div>
-                  <p class="card-text crop-text-2"><?php echo $result['Description'] ?></p>
-                  <div class="row">
-                    <a href="#" class="btn btn-outline-warning col-6" >Mark as in-stock</a>
-                    <a href="#" class="btn btn-outline-primary col-6" >Generate report</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <?php $result = $stmt->fetch(); ?>
-            <div class="col mt-2">
-              <div class="card">
-                <img class="card-img-top" src="images/product placeholders/product placeholder.jpg" alt="A placeholder image" >
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-8">
-                      <h5 class="card-title"><?php echo $result['Name'] ?></h5>
-                    </div>
-                    <div class="col-4">
-                      <h5 class="card-title">£<?php echo $result['PurchaseCost'] ?></h5>
-                    </div>
-                  </div>
-                  <p class="card-text crop-text-2"><?php echo $result['Description'] ?></p>
-                  <div class="row">
-                    <a href="#" class="btn btn-outline-warning col-6" >Mark as in-stock</a>
-                    <a href="#" class="btn btn-outline-primary col-6" >Generate report</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <?php $result = $stmt->fetch(); ?>
-            <div class="col mt-2">
-              <div class="card">
-                <img class="card-img-top" src="images/product placeholders/product placeholder sold out.jpg" alt="A placeholder image" >
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-8">
-                      <h5 class="card-title"><?php echo $result['Name'] ?></h5>
-                    </div>
-                    <div class="col-4">
-                      <h5 class="card-title">£<?php echo $result['PurchaseCost'] ?></h5>
-                    </div>
-                  </div>
-                  <p class="card-text crop-text-2"><?php echo $result['Description'] ?></p>
-                  <div class="row">
-                    <a href="#" class="btn btn-outline-warning col-6" >Mark as in-stock</a>
-                    <a href="#" class="btn btn-outline-primary col-6" >Generate report</a>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <?php 
+          $result = $stmt->fetch();
+          } ?>
           </div>
         </div>
 
         <div class="col-lg-2 d-none d-xl-block border mt-4"> <!-- Additional column for shopping cart -->
-
           <div class="row mt-2">
             <div class="col">
               <h3><i class="fa-solid fa-cart-shopping"></i>Add new product:</h3>
               <div class="row">
                 <div class="col">
-
+                  
                 </div>
               </div>
               <hr class="hr hr-blurry mt-2"/>
@@ -220,7 +67,7 @@
                   <input type="text" class="form-control" name="name" placeholder="Name">
                 </div>
                 <p></p> <!-- spacing smile -->
-                </div>
+                
                 <div class="form-group">
                   <label for="exampleFormControlTextarea1">Product Description:</label>
                   <textarea class="form-control" name="description" rows="3"></textarea>
@@ -245,10 +92,18 @@
                 </div>
     </form>
             </div>
+</div>
+            <div class="row mt-5 ms-3 me-3">
+              <form name="Remove New Product" action="removeProduct.php" method="post">
+                <h4>Delete Product:</h4>
+                <input type="text" class="form-control" name="delID" placeholder="ID to delete">
+                <input type="submit" name="submitDel" value="Confirm" class="btn btn-outline-success col-12 mt-2"></input>
+              </form>
+            </div>
           </div>
       </div>
     </div>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
