c7e1ebee8ab2d498578815f0d91ef5a0a2ffedc2
Matthew Gibson <2425530@dundee.ac.uk>
2023-12-02 20:08:44 +0000

    not brokey anymore :D

--- Work on/manager.php
+++ Work on/manager.php
@@ -2,11 +2,11 @@
 <html lang="en" dir="ltr">
   <!-- include head for page -->
   <?php include 'includes/head.php';?>
-<body>
+<body onload="testFunction(1, null)">
   <?php
   include 'includes/db.php';
   include 'includes/navbar.php';
-  include 'includes/productPageNav.php';
+  //include 'includes/productPageNav.php';
   ?>
     <div class="container-fluid">
       <div class="row">
@@ -18,66 +18,11 @@
               </h5>
             </div>
             <div class="col-6 d-flex justify-content-end align-items-center">
-              <form action="<?php echo "{$_SERVER['PHP_SELF']}";?>" class="form-check">
-                  Page <input type="text" value="<?php echo $currentPage;?>" size="1"> of <?php echo $numOfPages;?>
-              </form>
+              <p>temp</p>
             </div>
           </div>
           <div id="products" class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
-            <?php
-            // Generate Cards
-            while ($result != NULL) { ?>
-              <div class="col mt-2">
-                <div class="card bg-dark text-white">
-                  <img class="card-img-top" src="images/product placeholders/product placeholder.jpg" alt="A placeholder image">
-                  <div class="card-body">
-                    <div class="row">
-                      <div class="col-8">
-                        <h5 class="card-title"><?php echo $result['ProductID'], ': ', $result['Name'] ?></h5>
-                      </div>
-                      <div class="col-4">
-                        <h5 class="card-title">£<?php echo $result['PurchaseCost'] ?></h5>
-                      </div>
-                    </div>
-                    <p class="card-text crop-text-2"><?php echo $result['Description'] ?></p>
-                    <div class="row">
-                      <div class="btn-group" data-toggle="buttons">
-                        <a href="#" class="btn btn-outline-primary col-6" >Generate report</a>
-                        <a href="#" class="btn btn-outline-warning col-6" >Mark as in-stock</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <?php 
-            $result = $stmt->fetch();
-            } ?>
-          </div>
-          
-          <!-- navigation for prouct pages -->
-          <div class="row">
-            <div class="col-12 d-flex justify-content-between align-items-center my-1">
-                <a href="<?php echo "{$_SERVER['PHP_SELF']}?currentPage="; echo $currentPage-1;?>" class="btn btn-outline-success <?php if ($currentPage==0) {echo "disabled";}?>"><span class = "d-none d-sm-block">Previous</span> <span class = "d-block d-sm-none">&laquo;</span></a>
-                <div class="d-flex justify-content-center">
-                    <?php 
-                    $printedCurrent = False;
-                    $numOfPages = 20;
-                    $numOfButtons=9;
-                    for ($i=1; $i<$numOfButtons+1; $i++) {
-                      if (($numOfPages-$currentPage+$i < $numOfButtons )){
-                        echo "<a href=\"{$_SERVER['PHP_SELF']}?currentPage=", $numOfPages-($numOfButtons-$i) ,"\" class=\"me-1 btn btn-outline-success\">", $numOfPages-($numOfButtons-$i), "</a>";
-                      } else if ($printedCurrent != True){
-                        echo "<a href=\"{$_SERVER['PHP_SELF']}?currentPage=", $currentPage ,"\" class=\"me-1 btn btn-outline-success active\">", $currentPage, "</a>";
-                        $printedCurrent = True;
-                      } else if ($numOfPages>$numOfPages-$currentPage+$i) {
-                        echo "<a href=\"{$_SERVER['PHP_SELF']}?currentPage=", $numOfPages-($numOfButtons-$i) ,"\" class=\"me-1 btn btn-outline-success\">", $numOfPages-($numOfButtons-$i), "</a>";
-                      }
-                    } 
-                    ?>
-                </div>
-                <a href="<?php echo "{$_SERVER['PHP_SELF']}?currentPage="; echo $currentPage+1;?>" class="btn btn-outline-success <?php if ($currentPage==$numOfPages) {echo "disabled";}?>"><span class = "d-none d-sm-block">Next Page</span> <span class = "d-block d-sm-none">&raquo;</span></a>
-              </div>            
-            </div>
+            <!-- Content is generated using black magic from via jquery ajax & php -->
           </div>
 
         <div class="col-lg-2 d-none d-xl-block border mt-2"> <!-- Additional column for shopping cart -->
@@ -137,6 +82,8 @@
           </div>
       </div>
     </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="javascript/javascript madness.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
