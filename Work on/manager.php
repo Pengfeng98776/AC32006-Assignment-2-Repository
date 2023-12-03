56b3d490bd2970df6bb2d3bd58651a82ea62cb64
Matthew Gibson <2425530@dundee.ac.uk>
2023-12-03 13:16:31 +0000

    fixed column alignment

--- Work on/manager.php
+++ Work on/manager.php
@@ -1,90 +1,93 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <!-- include head for page -->
-  <?php include 'includes/head.php';?>
+<!-- include head for page -->
+<?php include 'includes/head.php';?>
 <body onload="testFunction(1, null)">
   <?php
   include 'includes/db.php';
   include 'includes/navbar.php';
-  //include 'includes/productPageNav.php';
   ?>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-xl-10">
-          <div class="row mt-2">
-            <div class="col-6 align-items-center">
-              <h5>
-                  Products we have
-              </h5>
-            </div>
-            <div class="col-6 d-flex justify-content-end align-items-center">
-              <p>temp</p>
-            </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xl-10">
+        <div class="row mt-2">
+          <div class="col-6 align-items-center">
+            <h5>
+              Products we have
+            </h5>
           </div>
-          <div id="products" class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
-            <!-- Content is generated using black magic from via jquery ajax & php -->
+          <div class="col-6 d-flex justify-content-end align-items-center">
+            <p>temp</p>
           </div>
+        </div>
+        <div id="products" class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
+          <!-- Content is generated using black magic from via jquery ajax & php -->
+        </div>
+      </div>
+      <div class="col-lg-2 d-none d-xl-block border mt-2"> <!-- Additional column for shopping cart -->
+        <div class="row mt-2">
+          <div class="col">
+            <h3><i class="fa-solid fa-cart-shopping"></i>Add new product:</h3>
+            <div class="row">
+              <div class="col">
 
-        <div class="col-lg-2 d-none d-xl-block border mt-2"> <!-- Additional column for shopping cart -->
-          <div class="row mt-2">
-            <div class="col">
-              <h3><i class="fa-solid fa-cart-shopping"></i>Add new product:</h3>
-              <div class="row">
-                <div class="col">
-                  
-                </div>
               </div>
-              <hr class="hr hr-blurry mt-2"/>
-              <div class="">
-                <h4>New product:</h4>
-                <p></p> <!-- spacing smile -->
+            </div>
+            <hr class="hr hr-blurry mt-2" />
+            <div class="">
+              <h4>New product:</h4>
+              <p></p> <!-- spacing smile -->
+            </div>
+            <form name="Add New Product" action="addProduct.php" method="post">
+              <p></p> <!-- spacing smile -->
+              <div class="form-group">
+                <label for="formGroupExampleInput">Product name:</label>
+                <input type="text" class="form-control" name="name" placeholder="Name">
               </div>
-              <form name="Add New Product" action="addProduct.php" method="post">
-                <p></p> <!-- spacing smile -->
-                <div class="form-group">
-                  <label for="formGroupExampleInput">Product name:</label>
-                  <input type="text" class="form-control" name="name" placeholder="Name">
-                </div>
-                <p></p> <!-- spacing smile -->
-                
-                <div class="form-group">
-                  <label for="exampleFormControlTextarea1">Product Description:</label>
-                  <textarea class="form-control" name="description" rows="3"></textarea>
-                </div>
-                <p></p> <!-- spacing smile -->
-                <div class="form-group">
-                  <label for="formGroupExampleInput">Purchase Cost:</label>
-                  <input type="text" class="form-control" name="purchaseCost" placeholder="price">
-                </div>
-                <p></p> <!-- spacing smile -->
-                <div class="form-group">
-                  <label for="formGroupExampleInput">Selling Price:</label>
-                  <input type="text" class="form-control" name="sellingPrice" placeholder="selling price">
-                </div>
-                <p></p> <!-- spacing smile -->
-                <div class="form-group">
-                  <label for="formGroupExampleInput">Current quantity:</label>
-                  <input type="text" class="form-control" name="stockQuantity" placeholder="quantity">
-                </div>
-                <div class="col mt-3 mb-2">
-                  <input type="submit" name="submit" value="Confirm" class="btn btn-outline-success col-12"></input>
-                </div>
-    </form>
-            </div>
-</div>
-            <div class="row mt-5 ms-3 me-3">
-              <form name="Remove New Product" action="removeProduct.php" method="post">
-                <h4>Delete Product:</h4>
-                <input type="text" class="form-control" name="delID" placeholder="ID to delete">
-                <input type="submit" name="submitDel" value="Confirm" class="btn btn-outline-success col-12 mt-2"></input>
-              </form>
-            </div>
+              <p></p> <!-- spacing smile -->
+
+              <div class="form-group">
+                <label for="exampleFormControlTextarea1">Product Description:</label>
+                <textarea class="form-control" name="description" rows="3"></textarea>
+              </div>
+              <p></p> <!-- spacing smile -->
+              <div class="form-group">
+                <label for="formGroupExampleInput">Purchase Cost:</label>
+                <input type="text" class="form-control" name="purchaseCost" placeholder="price">
+              </div>
+              <p></p> <!-- spacing smile -->
+              <div class="form-group">
+                <label for="formGroupExampleInput">Selling Price:</label>
+                <input type="text" class="form-control" name="sellingPrice" placeholder="selling price">
+              </div>
+              <p></p> <!-- spacing smile -->
+              <div class="form-group">
+                <label for="formGroupExampleInput">Current quantity:</label>
+                <input type="text" class="form-control" name="stockQuantity" placeholder="quantity">
+              </div>
+              <div class="col mt-3 mb-2">
+                <input type="submit" name="submit" value="Confirm" class="btn btn-outline-success col-12"></input>
+              </div>
+            </form>
           </div>
+        </div>
+        <div class="row mt-5 ms-3 me-3">
+          <form name="Remove New Product" action="removeProduct.php" method="post">
+            <h4>Delete Product:</h4>
+            <input type="text" class="form-control" name="delID" placeholder="ID to delete">
+            <input type="submit" name="submitDel" value="Confirm" class="btn btn-outline-success col-12 mt-2"></input>
+          </form>
+        </div>
       </div>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="javascript/javascript madness.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+      crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+      crossorigin="anonymous"></script>
 </body>
+
 </html>
